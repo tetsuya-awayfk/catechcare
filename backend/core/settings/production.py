faa43d8c2cb31304df328570445ca711c7f8ae39
tetsuya-afk <jccola9@gmail.com>
2026-05-11 03:12:47 +0800

    Temporarily enable DEBUG to diagnose 500 error

--- backend/core/settings/production.py
+++ backend/core/settings/production.py
@@ -2,7 +2,7 @@ from .base import *
 import os
 import dj_database_url
 
-DEBUG = False
+DEBUG = True
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 ALLOWED_HOSTS = ['api.catechcare.com', 'catechcare-backend.onrender.com', 'localhost', '127.0.0.1', 'catechcare.onrender.com']
 
